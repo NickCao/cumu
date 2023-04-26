b82810bbc68afcdd392b0430e0fb0ea85c373ded
Nick Cao <nickcao@nichi.co>
2023-04-26 14:43:49 +0800

    add cudart to build inputs

--- flake.nix
+++ flake.nix
@@ -3,9 +3,19 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
-  outputs = { self, nixpkgs, flake-utils, ... }:
+  outputs = { nixpkgs, flake-utils, ... }:
     flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = import nixpkgs { inherit system; }; in {
-        devShells.default = pkgs.mkShell { };
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          config.allowUnfree = true;
+        };
+      in
+      with pkgs;{
+        devShells.default = mkShell {
+          buildInputs = [
+            cudaPackages_12.cuda_cudart
+          ];
+        };
       });
 }
