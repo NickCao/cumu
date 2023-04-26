12ed640c0e2c7db4934d2d50619d2bed8b708372
Nick Cao <nickcao@nichi.co>
2023-04-26 18:37:00 +0800

    add basic smoketest

---
+++ smoketest/smoketest.cu
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <cuda_runtime_api.h>
+
+#define N 10000000
+
+__global__ void vector_add(float *out, float *a, float *b, int n) {
+    for(int i = 0; i < n; i++){
+        out[i] = a[i] + b[i];
+    }
+}
+
+int main(){
+    float *a, *b, *out;
+
+    // Allocate memory
+    a   = (float*)malloc(sizeof(float) * N);
+    b   = (float*)malloc(sizeof(float) * N);
+    out = (float*)malloc(sizeof(float) * N);
+
+    // Initialize array
+    for(int i = 0; i < N; i++){
+        a[i] = 1.0f; b[i] = 2.0f;
+    }
+
+    vector_add<<<1,1>>>(out, a, b, N);
+    cudaError_t err = cudaGetLastError();
+    std::cout << cudaGetErrorString(err) << std::endl;
+}
