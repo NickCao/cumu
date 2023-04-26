695c018495915d92e3dc18913afbfbf4d031d423
Nick Cao <nickcao@nichi.co>
2023-04-26 15:37:07 +0800

    pretend to implement cuda 12

--- cuda/src/lib.rs
+++ cuda/src/lib.rs
@@ -1,8 +1,14 @@
 #![allow(non_snake_case)]
 use cuda_sys::*;
-use std::os::raw::c_uint;
+use std::os::raw::{c_int, c_uint};
 
 #[no_mangle]
 pub unsafe extern "C" fn cuInit(_flags: c_uint) -> CUresult {
+    CUresult::CUDA_SUCCESS
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn cuDriverGetVersion(version: *mut c_int) -> CUresult {
+    *version = 1000 * 12 + 10 * 0;
     CUresult::CUDA_SUCCESS
 }
