de670b3d44d83115d4f2181c1f7a561bbed50d62
Nick Cao <nickcao@nichi.co>
2023-04-26 16:55:31 +0800

    no longer need to expose symbols

--- cuda/src/lib.rs
+++ cuda/src/lib.rs
@@ -21,6 +21,7 @@ pub unsafe extern "C" fn cuGetProcAddress_v2(
     *pfn = match symbol {
         "cuInit" => cuInit as _,
         "cuGetProcAddress" => cuGetProcAddress_v2 as _,
+        "cuDriverGetVersion" => cuDriverGetVersion as _,
         _ => stub as _,
     };
     if !status.is_null() {
@@ -29,18 +30,17 @@ pub unsafe extern "C" fn cuGetProcAddress_v2(
     CUresult::CUDA_SUCCESS
 }
 
-#[no_mangle]
-pub unsafe extern "C" fn cuInit(_flags: c_uint) -> CUresult {
+unsafe extern "C" fn cuInit(_flags: c_uint) -> CUresult {
     CUresult::CUDA_SUCCESS
 }
 
-#[no_mangle]
-pub unsafe extern "C" fn cuDriverGetVersion(version: *mut c_int) -> CUresult {
+unsafe extern "C" fn cuDriverGetVersion(version: *mut c_int) -> CUresult {
     *version = 1000 * 12 + 10 * 0;
     CUresult::CUDA_SUCCESS
 }
 
 #[no_mangle]
-pub unsafe extern "C" fn stub() {
+pub unsafe extern "C" fn stub() -> CUresult {
     eprintln!("stub");
+    CUresult::CUDA_ERROR_UNKNOWN
 }
